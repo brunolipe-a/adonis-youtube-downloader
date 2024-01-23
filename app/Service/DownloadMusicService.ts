4c45348e0bb0e33b1bf9204665609d7a2fc79df2
Bruno <bfaa1999@gmail.com>
2024-01-23 12:24:38 -0300

    fix: try/catch youtube download music

--- app/Service/DownloadMusicService.ts
+++ app/Service/DownloadMusicService.ts
@@ -9,13 +9,18 @@ export class DownloadMusicService {
   constructor(protected generateMp3Service: GenerateMp3Service) {}
 
   public async handle(music: Music, path: string) {
-    const stream = YouTubeDownload(music.videoUrl, {
-      quality: 'highestaudio',
-      filter: 'audioonly',
-    })
-
     const sanitizedMusicTitle = music.title.replace('/', '-')
 
-    await this.generateMp3Service.handle(stream, `${path}/${sanitizedMusicTitle}.mp3`)
+    try {
+      const stream = YouTubeDownload(music.videoUrl, {
+        quality: 'highestaudio',
+        filter: 'audioonly',
+      })
+
+      await this.generateMp3Service.handle(stream, `${path}/${sanitizedMusicTitle}.mp3`)
+    } catch (error) {
+      console.error(`Houve um problema com a música ${music.title}`)
+      console.error(error)
+    }
   }
 }
